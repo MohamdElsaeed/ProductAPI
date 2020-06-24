c2a273519e1472c8d145775ce0698e9d1d6c752e
MohamedElsaaed <devmohamed6@gamil.com>
2020-06-24 20:27:43 +0200

    created endpoints route resources

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,34 @@
 
 use Illuminate\Http\Request;
 
- 
+    /**
+     * Buyers
+     */
+
+    Route::resource('buyers', 'Buyer\BuyerController',['only'=>['show','index']]);
+
+    /**
+     * Sellers
+     */
+
+    Route::resource('sellers', 'Seller\SellerController',['only'=>['show','index']]);
+    /**
+     * Categories
+     */
+
+    Route::resource('categories', 'Category\CategoryController',['except'=>['create','edit']]);
+    /**
+     * Transactions
+    */
+    
+    Route::resource('tranasctions', 'Transaction\TransactionController',['only'=>['show','index']]);
+    /**
+     * Products
+     */
+        Route::resource('products', 'Product\ProductController',['only'=>['show','index']]);
+
+    /**
+     * Users
+     */
+
+    Route::resource('users', 'User\UserController',['except'=>['create','edit']]);
